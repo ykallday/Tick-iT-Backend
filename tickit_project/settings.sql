403aee3ebab395fdf8d0fe3afe28391f2c739f6c
Yael Kaufman <ylkauf@gmail.com>
2023-03-31 07:28:18 -0700

    Restart

--- tickit_project/settings.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE tickit;
-CREATE USER tickituser WITH PASSWORD 'tickit';
-GRANT ALL PRIVILEGES ON DATABASE tickit TO tickituser;
--- tickit_project/tickit/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TickitConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tickit'
